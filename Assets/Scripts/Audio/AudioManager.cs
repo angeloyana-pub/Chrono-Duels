b318a97410f8958c1af1ed979f65e799e5c0e12d
Angelo Yana <angeloyana.pub@gmail.com>
2025-10-01 19:00:31 +0800

    10-01-2025 - 2

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -13,14 +13,14 @@ public class AudioManager : MonoBehaviour
 
     public void PlayOverworldClip()
     {
-        _audioSource.Pause();
+        // _audioSource.Pause();
         _audioSource.clip = _overworldClip;
         _audioSource.Play();
     }
 
     public void PlayBattleClip()
     {
-        _audioSource.Pause();
+        // _audioSource.Pause();
         _audioSource.clip = _battleClip;
         _audioSource.Play();
     }
